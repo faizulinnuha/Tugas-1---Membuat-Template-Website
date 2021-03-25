87d0513f44def69bf530d3a5e5c75d5d0405a3ca
faiz ulin nuha <32800617+faizulinnuha@users.noreply.github.com>
2021-03-25 13:42:23 +0700

    Add files via upload

---
+++ detail.php
@@ -0,0 +1,19 @@
+	<div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Artikel</h1>
+        </div>
+    </div>
+
+    <div class="container">
+    	<p>Ini adalah deskripsi singkat tentang halaman artikel ini. Lihat selengkapnya untuk membaca semua konten.</p>
+    	<p>Ini adalah paragraf ke dua</p>
+    	<p>Ini adalah paragraf ke tiga</p>
+    	<p>Ini adalah paragraf ke empat</p>
+    	<p>Ini adalah paragraf ke lima</p>
+    	<p>Ini adalah paragraf ke enam</p>
+    	<p>Ini adalah paragraf ke tujuh</p>
+    	<p>Ini adalah paragraf ke delapan</p>
+    	<p>Ini adalah paragraf ke sembilan</p>
+    	<p>Ini adalah paragraf ke sepuluh</p>
+    	<p>Paragraf terakhir dalam artikel ini</p>
+    </div>
---
+++ kontak.php
@@ -0,0 +1,48 @@
+	<div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Kontak</h1>
+        </div>
+    </div>
+
+	<section class="mbr-section mb-5 form1">
+	    <div class="container">
+	        <div class="row justify-content-center">
+	            <div class="media-container-column col-lg-8">
+	                    <div class="mb-4 mt-3 text-center">
+	                        Hubungi saya dengan mengisi formulir di bawah ini!
+	                    </div>
+	            
+	                    <form class="mbr-form" method="post">
+	                        <div class="row row-sm-offset">
+	                            <div class="col-md-4 multi-horizontal" data-for="name">
+	                                <div class="form-group">
+	                                    <label class="form-control-label mbr-fonts-style display-7" for="name-form1-9i">Nama</label>
+	                                    <input type="text" class="form-control" name="name" data-form-field="Name" required="" id="name-form1-9i">
+	                                </div>
+	                            </div>
+	                            <div class="col-md-4 multi-horizontal" data-for="email">
+	                                <div class="form-group">
+	                                    <label class="form-control-label mbr-fonts-style display-7" for="email-form1-9i">Email</label>
+	                                    <input type="email" class="form-control" name="email" data-form-field="Email" required="" id="email-form1-9i">
+	                                </div>
+	                            </div>
+	                            <div class="col-md-4 multi-horizontal" data-for="phone">
+	                                <div class="form-group">
+	                                    <label class="form-control-label mbr-fonts-style display-7" for="phone-form1-9i">No. Telepon</label>
+	                                    <input type="tel" class="form-control" name="phone" data-form-field="Phone" id="phone-form1-9i">
+	                                </div>
+	                            </div>
+	                        </div>
+	                        <div class="form-group" data-for="message">
+	                            <label class="form-control-label mbr-fonts-style display-7" for="message-form1-9i">Isi Pesan</label>
+	                            <textarea type="text" class="form-control" name="message" rows="7" data-form-field="Message" id="message-form1-9i"></textarea>
+	                        </div>
+	            
+	                        <span class="input-group-btn">
+	                            <button href="" type="submit" class="btn btn-primary btn-form display-4">KIRIM</button>
+	                        </span>
+	                    </form>
+	            </div>
+	        </div>
+	    </div>
+	</section>
